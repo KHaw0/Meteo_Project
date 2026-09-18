b29f2c562d53f7fdf23c41fe16ccb68d01430dc6
Zerox <natapoom7kp5@gmail.com>
2026-09-19 00:29:10 +0700

    Create PaintBg.java

---
+++ PaintBg.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class PaintBg extends JLabel{
+
+    PaintBg() {
+        setBackground(Color.BLACK);
+        
+    }
+
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+    }
+}
